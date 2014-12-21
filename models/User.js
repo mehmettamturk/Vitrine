b3f9981b4f8ec4983677faffe64b0a5e4dc60cdc
Mert Dogar <mertdogar@gmail.com>
2014-12-21 02:16:25 +0200

    Add email fields to the user schema.

--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ var userSchema = mongoose.Schema({
     username: {type: String, unique: true},
     displayName: {type: String},
     password: String,
+    email: String,
     permissions: [String]
 });
 
