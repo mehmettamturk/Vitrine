bb94697409a3e0e57e23b26724a0dfb88bcf80d7
Mehmet Tamturk <mehmet.tamturk@startup.kitchen>
2015-01-21 20:27:10 +0200

    Add basic product list.

--- public/js/controllers/HomepageController.js
+++ public/js/controllers/HomepageController.js
@@ -1,3 +1,7 @@
-vitrine.controller("HomepageController", function($scope) {
+vitrine.controller("HomepageController", function($scope, $http) {
     console.log('HomepageController...');
+    $http.get('/product/all').success(function(data) {
+        $scope.products = data;
+        console.log(data)
+    });
 });
